0b4584459178655e382f7a0508a6452a636247c9
Marcos Domingues <marcos.domingues@poncetech.com.br>
2023-10-11 08:18:13 -0300

    (config) theme and ts styles

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,16 @@
+'use client'
+
 import { PropsWithChildren } from 'react'
 
 import GlobalStyles from '@/styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/theme'
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       {children}
-    </>
+    </ThemeProvider>
   )
 }
