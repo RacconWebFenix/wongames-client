caadc5c83e757b0f32b750c7781034ea502201de
Marcos Domingues <dfenixweb@gmail.com>
2023-11-01 12:06:30 -0300

    Adjusting Heading to contain size and lineColors

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,29 +6,76 @@ import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from '../Logo'
 import * as S from './styles'
+import Button from '../Button'
+import MediaMatch from '../MediaMatch'
 
-const Menu = () => {
+export type MenuProps = {
+  username?: string
+}
+
+const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <S.Wrapper>
-      <S.IconWrapper>
-        <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
-      </S.IconWrapper>
+      <MediaMatch $lessThan="medium">
+        <S.IconWrapper>
+          <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
+        </S.IconWrapper>
+      </MediaMatch>
+
       <S.LogoWrapper>
         <Logo $hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch $greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
         </S.IconWrapper>
+
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+
+        {!username && (
+          <MediaMatch $greaterThan="medium">
+            <Button>Sign In</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} $isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+          {username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+        {!username && (
+          <>
+            <S.RegisterBox>
+              <Button $fullWidth size="large">
+                Log in Now
+              </Button>
+              <span>or</span>
+              <S.CreateAccount href="#" title="Sign Up">
+                Sign Up
+              </S.CreateAccount>
+            </S.RegisterBox>
+          </>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   )
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, screen } from '@testing-library/react'
-
 import Menu from '.'
 import { renderWithTheme } from '@/utils/tests/helpers'
 
@@ -13,7 +12,7 @@ describe('<Menu />', () => {
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
   })
 
-  it('should rendle the open/close mobile menu', () => {
+  it('should hendle the open/close mobile menu', () => {
     renderWithTheme(<Menu />)
 
     // selecioar o menu full
@@ -32,5 +31,30 @@ describe('<Menu />', () => {
     fireEvent.click(screen.getByLabelText(/close menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+  it('should show register box', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByText(/Log in Now/i)).toBeInTheDocument()
+    expect(screen.getByText(/Sign Up/i)).toBeInTheDocument()
+  })
+  it('should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu username="Marcos" />)
+
+    expect(screen.queryByText(/My account/i)).toBeInTheDocument()
+    expect(screen.queryByText(/Wishlist/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/Log in Now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Sign Up/i)).not.toBeInTheDocument()
+  })
+
+  it('should show wishlist and account when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText(/My account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument()
+
+    expect(screen.getByText(/Log in Now/i)).toBeInTheDocument()
+    expect(screen.getByText(/Sign Up/i)).toBeInTheDocument()
   })
 })
